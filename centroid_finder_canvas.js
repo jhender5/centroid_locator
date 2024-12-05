7ef7e43de2d5a7a69fd427df5b40299a61aaa414
jhender5 <jasper.henderson@uvm.edu>
2024-12-05 13:48:45 -0500

    Can click on button and shape follows mouse

--- centroid_finder_canvas.js
+++ centroid_finder_canvas.js
@@ -156,7 +156,7 @@ class CompositeShape{
         this.#shapes = new Array(0)
     }
 
-    set add_shape(shape){
+    addShape(shape){
         this.#shapes.push(shape);
     }
 
@@ -237,6 +237,7 @@ class Button{
     #rectBG;
     #icon;
     #iconScaler=0.7;
+    #iconType=undefined;
     #triangleOrientation =1;
     constructor(xPos, yPos, size, iconType) {
         this.#rectBG = new Rectangle(xPos, yPos, size, size);
@@ -244,12 +245,19 @@ class Button{
         switch (iconType){
             case "rectangle":
                 this.#icon = new Rectangle(xPos, yPos, iconSize, iconSize);
+                this.#iconType=iconType;
                 break;
             case "circle":
-                this.#icon = new Circle(xPos, yPos, iconSize);
+                this.#icon = new Circle(xPos, yPos, iconSize/2);
+                this.#iconType=iconType;
                 break;
             case "triangle":
                 this.#icon = new Triangle(xPos, yPos, iconSize, iconSize, this.#triangleOrientation);
+                this.#iconType=iconType;
+                break;
+            case "none":
+                this.#iconType = "none";
+                break;
         }
     }
 
@@ -267,23 +275,23 @@ class Button{
     }
 
     draw(context){
+        context.fillStyle="white"
         let rectXPos=this.#rectBG.xPos - this.#rectBG.width/2;
         let rectYpos=this.#rectBG.yPos - this.#rectBG.height/2;
         context.fillRect(rectXPos, rectYpos, this.#rectBG.width, this.#rectBG.height);
+        context.fillStyle="red"
+        if (this.#iconType === "rectangle"){
+            let rectXPos= this.#icon.xPos - this.#icon.width/2;
+            let rectYpos=this.#icon.yPos - this.#icon.width/2;
+            context.fillRect(rectXPos, rectYpos, this.#icon.width, this.#icon.height);
 
-        if (typeof this.#icon === Rectangle){
-                let rectXPos= this.#icon.xPos - this.#icon.width/2;
-                let rectYpos=this.#icon.yPos - this.#icon.width/2;
-                context.fillRect(rectXPos, rectYpos, this.#icon.width, this.#icon.height);
-
-        } else if (typeof this.#icon === Circle) {
+        } else if (this.#iconType === "circle") {
             context.beginPath();
-            context.beginPath();
-            context.arc(this.#icon.xPos, -this.#icon.yPos, this.#icon.radius, 0, Math.PI * 2);
+            context.arc(this.#icon.xPos, this.#icon.yPos, this.#icon.radius, 0, Math.PI * 2);
             context.fill();
             context.closePath();
 
-        } else if (typeof this.#icon === Triangle){
+        } else if (this.#iconType === "triangle"){
             context.beginPath()
             const yCenter = this.#icon.yPos;
             const xCenter = this.#icon.xPos;
@@ -317,59 +325,221 @@ class Button{
 
         }
     }
+
+    intersects(point){
+        return point.x >= this.#rectBG.xPos-this.#rectBG.width/2
+            && point.x <= this.#rectBG.xPos+this.#rectBG.width/2
+            && point.y >= this.#rectBG.yPos-this.#rectBG.height/2
+            && point.y <= this.#rectBG.yPos+this.#rectBG.height/2;
+    }
 }
 
-const myCanvas=document.getElementById("centroidCanvas");
-const canvasCtx=myCanvas.getContext('2d');
-const uiPos = [0,0];
+class graph{
+    #xPos;
+    #yPos;
+    #width;
+    #height;
+    constructor(xPos, yPos, width, height) {
+        this.#xPos = xPos;
+        this.#yPos = yPos;
+        this.#width = width;
+        this.#height = height;
+    }
 
-let mouseX=0;
-let mouseY=0;
+    intersects(point){
+        return point.x > this.#xPos
+            && point.x < this.#xPos+this.#width
+            && point.y > this.#yPos
+            && point.y < this.#yPos+this.#width
+    }
 
+    draw(context){
+        context.fillRect(this.#xPos, this.#yPos, this.#width, this.#height);
+    }
+}
+
+//This function draws the user interface and canvas
+function draw(){
+
+    bgCanvasCtx.clearRect(0, 0, bgCanvas.width, bgCanvas.height)
+    addTriangleButton.draw(bgCanvasCtx);
+    addRectButton.draw(bgCanvasCtx);
+    addCircleButton.draw(bgCanvasCtx);
+
+    graphCanvasCTX.fillStyle="white";
+    shapeGraph.draw(graphCanvasCTX);
+
+    //TODO: render composite shape
+
+    //TODO: make UI for modifying shapes
+
+    //TODO: move shapes with mouse if a shape has been clicked
+    if(mouseDomain === 'bg') {
+        if(placingShape === 'circle') {
+            bgCanvasCtx.beginPath();
+            bgCanvasCtx.arc(bgMouseX, bgMouseY, 15, 0, Math.PI * 2);
+            bgCanvasCtx.fillStyle = 'pink';
+            bgCanvasCtx.fill();
+            bgCanvasCtx.closePath();
+        } else if(placingShape === 'rectangle'){
+            bgCanvasCtx.fillStyle = 'pink';
+            bgCanvasCtx.fillRect(bgMouseX-15, bgMouseY-15, 30, 30);
+        }else if(placingShape=== 'triangle'){
+            drawTriangle(bgCanvasCtx, bgMouseX, bgMouseY, 30, 30, 1, 'pink');
+        }
+
+    } else if(mouseDomain === 'graph'){
+        if(placingShape === 'circle') {
+            graphCanvasCTX.beginPath();
+            graphCanvasCTX.arc(graphCanvasMouseX, graphCanvasMouseY, 15, 0, Math.PI * 2);
+            graphCanvasCTX.fillStyle = 'pink';
+            graphCanvasCTX.fill();
+            graphCanvasCTX.closePath();
+        } else if(placingShape === 'rectangle'){
+            graphCanvasCTX.fillStyle = 'pink';
+            graphCanvasCTX.fillRect(graphCanvasMouseX-15, graphCanvasMouseY-15, 30, 30);
+        } else if(placingShape=== 'triangle'){
+            drawTriangle(graphCanvasCTX, graphCanvasMouseX, graphCanvasMouseY, 30, 30, 1, 'pink');
+        }
+    }
+    requestAnimationFrame(draw);
+}
+
+function drawTriangle(context, xCenter, yCenter, width, height, orientation, color){
+    context.beginPath()
+    context.fillStyle=color
+            switch (orientation) {
+                case 1:
+                    context.moveTo(xCenter - width / 2, yCenter - height / 2);
+                    context.lineTo(xCenter - width / 2, yCenter + height / 2);
+                    context.lineTo(xCenter + width / 2, yCenter + height / 2)
+                    context.closePath()
+                    break;
+                case 2:
+                    context.moveTo(xCenter - width / 2, yCenter + height / 2);
+                    context.lineTo(xCenter + width / 2, yCenter + height / 2);
+                    context.lineTo(xCenter + width / 2, yCenter - height / 2)
+                    context.closePath()
+                    break;
+                case 3:
+                    context.moveTo(xCenter + width / 2, yCenter - height / 2);
+                    context.lineTo(xCenter - width / 2, yCenter - height / 2);
+                    context.lineTo(xCenter - width / 2, yCenter + height / 2)
+                    context.closePath()
+                    break;
+                case 4:
+                    context.moveTo(xCenter + width / 2, yCenter + height / 2);
+                    context.lineTo(xCenter + width / 2, yCenter - height / 2);
+                    context.lineTo(xCenter - width / 2, yCenter - height / 2)
+                    context.closePath()
+                    break;
+            }
+            context.fill();
+}
+
+
+
+const bgCanvas=document.getElementById("centroidCanvas");
+const bgCanvasCtx=bgCanvas.getContext('2d');
+
+bgCanvas.width=window.innerWidth;
+bgCanvas.height=window.innerHeight;
+
+
+//TODO: figure out why canvas not show up
+let graphCanvas=document.getElementById("graphSpace");
+let graphCanvasCTX=graphCanvas.getContext('2d');
+
+let shapeGraphDimensions={x:window.innerWidth*(1/10),
+                                y:window.innerHeight*(2/10),
+                                width:window.innerWidth*(8/10),
+                                height:window.innerHeight*(8/10)};
+
+let shapeGraph = new graph(0,0, shapeGraphDimensions.width, shapeGraphDimensions.height);
+
+graphCanvas.style.top=shapeGraphDimensions.y+'px';
+graphCanvas.style.left=shapeGraphDimensions.x+'px';
+graphCanvas.width=shapeGraphDimensions.width;
+graphCanvas.height=shapeGraphDimensions.height;
+
+//Mouse action variables
+let bgMouseX=0;
+let bgMouseY=0;
+let graphCanvasMouseX=0;
+let graphCanvasMouseY=0;
 let placingShape="none";
-let addTriangleButton= new Button(20, 20, 30, "triangle");
-let addRectButton=new Button(20, 60, 30, "rectangle");
-let addCircleButton=new Button(20, 100, 30, "circle");
+let mouseDomain="none"
 
-myCanvas.addEventListener("mousemove", function (info){
-    mouseX=info.offsetX;
-    mouseY=info.offsetY;
+//Shape drawing variables
+let drawnShapes = new CompositeShape();
+let triangleOrientation = 1;
+
+//UI variables
+let shapeButtonSize=(bgCanvas.height+bgCanvas.width)/40;
+let shapeButtonXOffset=bgCanvas.width/20;
+let shapeButtonYOffest=bgCanvas.height/20;
+let addTriangleButton= new Button(shapeButtonXOffset, shapeButtonYOffest, shapeButtonSize, "triangle");
+let addRectButton=new Button(shapeButtonXOffset, shapeButtonYOffest*3, shapeButtonSize, "rectangle");
+let addCircleButton=new Button(shapeButtonXOffset, shapeButtonYOffest*5, shapeButtonSize, "circle");
+
+
+
+//event listeners
+bgCanvas.addEventListener("mousemove", function (info){
+    bgMouseX=info.x;
+    bgMouseY=info.y;
 })
 
-myCanvas.addEventListener("click", function (info){
-    let point={x:info.offsetX, y:info.offsetY};
-    if(isInsideRect(point, addTriangleButton, canvasCtx)){
-        placingShape = "triangle"
-    } else if(isInsideRect(point, addRectButton, canvasCtx)){
-        placingShape = "rectangle"
-    } else if (isInsideRect(point, addCircleButton, canvasCtx)){
-        placingShape = "circle"
+bgCanvas.addEventListener("mouseenter", function (){
+    mouseDomain="bg"
+})
+
+graphCanvas.addEventListener("mouseenter", function (){
+    mouseDomain="graph"
+})
+
+window.addEventListener("mouseleave", function (){
+    mouseDomain='none'
+})
+
+graphCanvas.addEventListener("mousemove", function (info){
+    graphCanvasMouseX=info.offsetX;
+    graphCanvasMouseY=info.offsetY;
+})
+
+graphCanvas.addEventListener("click", function (info){
+    graphCanvasMouseX=info.offsetX
+    graphCanvasMouseY=info.offsetY
+    if(placingShape === "none"){
+    } else if(placingShape === "triangle"){
+        drawnShapes.addShape(Triangle(graphCanvasMouseX,graphCanvas.width-graphCanvasMouseY, 30, 30, triangleOrientation));
+    } else if(placingShape === "rectangle"){
+        drawnShapes.addShape(Rectangle(graphCanvasMouseX, graphCanvas.width-graphCanvasMouseY, 30, 30));
+    } else if(placingShape === "circle"){
+        drawnShapes.addShape(Circle(graphCanvasMouseX, graphCanvas.width-graphCanvasMouseY, 15));
     }
 })
 
-function isInsideRect(point, rect, canvas){
-    const xPos=rect.xPos;
-    const yPos=canvas.height-rect.yPos;
-    const width = rect.width;
-    const height = rect.height;
 
-    return point.x >= xPos-width/2 && point.x <= xPos+width/2 && point.y >= yPos-height/2 && point.y <= yPos+height/2;
-}
+bgCanvas.addEventListener("click", function (info){
+    bgMouseX=info.offsetX;
+    bgMouseY=info.offsetY;
+    let point={x:info.offsetX, y:info.offsetY};
 
-function drawRectangle(rect, context, canvas){
-    let rectXPos=rect.xPos - rect.width/2;
-    let rectYpos=rect.yPos - rect.height/2;
-    context.fillRect(rectXPos, rectYpos, rect.width, rect.height);
-}
-
-function draw(){
-    canvasCtx.clearRect(0, 0, myCanvas.width, myCanvas.height)
-    canvasCtx.fillStyle="white"
-    addTriangleButton.draw(canvasCtx);
-    addRectButton.draw(canvasCtx);
-    addCircleButton.draw(canvasCtx);
-
-    requestAnimationFrame(draw);
-}
+    if(addTriangleButton.intersects(point)){
+         if(placingShape === "triangle"){
+            placingShape = "none";
+        } else placingShape = "triangle";
+    } else if(addRectButton.intersects(point)){
+         if(placingShape === "rectangle"){
+            placingShape = "none";
+        } else placingShape = "rectangle";
+    } else if (addCircleButton.intersects(point)){
+        if(placingShape === "circle"){
+            placingShape = "none";
+        } else placingShape = "circle"
+    }
+    console.log(placingShape);
+})
 
 draw();
